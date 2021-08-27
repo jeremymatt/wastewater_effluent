41ecd7165d233b95b30bacd75de22b1d7a077e04
Jeremy Matt <jeremy.matt@gmail.com>
2021-08-27 09:22:45 -0400

    Driver updates

--- driver.py
+++ driver.py
@@ -15,49 +15,121 @@ import SOM
 
 
 cd = os.getcwd()
-output_dir = os.path.join(cd,'Output')
-if not os.path.isdir(output_dir):
-    os.makedirs(output_dir)
+root_output_dir = os.path.join(cd,'Output')
 
 
 fn = 'VTDEC_PFAS_Influent_8-5-2021_Sydney_Adams.csv'
 data_df = pd.read_csv(fn)
 
 label_header = 'Influent Type'
+# label_header = 'Site'
 
+  
+labels = data_df[label_header]    
+    
 if label_header == 'Influent Type':
     headers_to_drop = [
         'Site',
         'Date']
+    legend_text = {
+        'Industrial Discharge':'ID',
+        'Landfill Leachate':'LL',
+        'Residential Only':'RO'}
 else:
     headers_to_drop = [
         'Influent Type',
         'Date']
     
+    label_set = set(data_df[label_header])
     
+    legend_text = {}
+    for label in label_set:
+        legend_text[label] = label
+    # legend_text = {
+    #     'Industrial Discharge':'ID',
+    #     'Landfill Leachate':'LL',
+    #     'Residential Only':'RO'}
 
 headers_to_keep = [header for header in data_df.keys() if header not in headers_to_drop]
 
 data_df = data_df[headers_to_keep]
 
 
-selected_features = SOM.get_nonsingular_cols(data_df)
+nonsingular_features = SOM.get_nonsingular_cols(data_df)
+data_df = data_df[nonsingular_features]
+
+data_labels = [key for key in data_df.keys() if key != label_header]
+
+norm_data = True
+if norm_data:
+    X = SOM.min_max_norm(data_df,data_labels)
+else:
+    X = data_df
 
 
-# singular_features = [feat for feat in headers_to_keep if not feat in selected_features]
-# print('\nWARNING: The following features have only one value:')
-# if len(singular_features)>0:
-#     for feat in singular_features:
-#         print('    {}'.format(feat))
-#     print('\n')
-# else:
-#     print('No features have')
-
-label_set = set(data_df[label_header])
-
-legend_text = {
-    'Industrial Discharge':'ID',
-    'Landfill Leachate':'LL',
-    'Residential Only':'RO'}
 
 
+
+
+#Set the grid size
+grid_size = [50,50]
+# grid_size = [4,4]
+#Set the starting learning rate and neighborhood size
+alpha = 0.9
+# neighborhood_size = 4
+neighborhood_size = int(grid_size[0]/4)
+# neighborhood_size = 1
+#Set the number of training epochs
+num_epochs = 500
+
+load_trained = False
+toroidal = True
+distance = 'euclidean'
+
+for i in range(3):
+    #Init a SOM object
+    SOM_model = SOM.SOM(grid_size,X,label_header,alpha,neighborhood_size,toroidal,distance)
+    
+    #Train the SOM
+    
+    output_dir = os.path.join(root_output_dir,f'run{i}')
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    if load_trained:
+        weights_dir = r'SOM_output\run10_keep'
+        weights_dir = weights_dir.split('\\')
+        directory = cd
+        for folder in weights_dir:
+            directory = os.path.join(directory,folder)
+            
+        fn = 'weights.pkl'
+        
+        SOM_model.load_weights(directory,fn)
+        
+        
+        
+            
+    else:
+        SOM_model.train(num_epochs)
+        SOM_model.save_weights(output_dir,'weights.pkl')
+    
+    SOM_model.plot_weight_hist(output_dir)
+    
+    #Plot the samples to the grid
+    # SOM_model.plot_samples()
+    
+    #Calculate the U-matrix differences
+    SOM_model.calc_u_matrix()
+    #boolean flag to include D in the u-matrix calculations
+    include_D = False
+    #Plot the u-matrix
+    sample_vis='labels'
+    sample_vis='symbols'
+    
+    SOM_model.plot_u_matrix(include_D,output_dir=output_dir,labels=labels,sample_vis=sample_vis,legend_text=legend_text)
+    plane_vis='u_matrix'
+    plane_vis='weights'
+    #Plot the feature planes
+    SOM_model.plot_feature_planes(output_dir,labels=labels,sample_vis=sample_vis,plane_vis=plane_vis,legend_text=legend_text)
+    
